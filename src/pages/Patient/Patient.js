2afacc263655d054f93526ccb75b5361ec3b53aa
Dro <lorddro1532@gmail.com>
2020-05-12 00:00:07 +0100

    Implemened Dashboard Controller Color Change

--- src/pages/Patient/Patient.js
+++ src/pages/Patient/Patient.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import home from './../../Assets/svg/home.svg';
+import activeHome from './../../Assets/svg/active-home.svg';
+import activeInfo from './../../Assets/svg/active-info.svg';
+import activeMessage from './../../Assets/svg/active-message.svg';
+import activeProfile from './../../Assets/svg/active-profile.svg';
+import circle from './../../Assets/svg/circle.svg';
 import info from './../../Assets/svg/info.svg';
 import message from './../../Assets/svg/message.svg';
 import profile from './../../Assets/svg/profile.svg';
@@ -24,10 +29,47 @@ class Patient extends Component{
       <div className="patient-container">
 
         <div className="dashboard">
-          <a onClick={()=> this.onLinkClick('home')} href="#"><img src={home} alt='home-icon'></img></a>
-          <a onClick={()=> this.onLinkClick('info')} href="#"><img src={info} alt='info-icon'></img></a>
-          <a onClick={()=> this.onLinkClick('message')} href="#"><img src={message} alt='mesage-icon'></img></a>
-          <a onClick={()=> this.onLinkClick('profile')} href="#"><img src={profile} alt='profile-icon'></img></a>
+          <div className="dashboard-control">
+            {
+              this.state.page==='home'
+              ? <>
+                <a onClick={()=> this.onLinkClick('home')} href="#"><img src={activeHome} alt='home-icon'></img></a>
+                <span><img className="circle" src={circle} alt='circle-icon'></img></span>
+              </>
+              : <a onClick={()=> this.onLinkClick('home')} href="#"><img src={home} alt='home-icon'></img></a>
+            }
+          </div>
+          <div className="dashboard-control">
+            {
+              this.state.page==='info'
+              ? <>
+                <a onClick={()=> this.onLinkClick('info')} href="#"><img src={activeInfo} alt='info-icon'></img></a>
+                <span><img className="circle" src={circle} alt='circle-icon'></img></span>
+              </>
+              : <a onClick={()=> this.onLinkClick('info')} href="#"><img src={info} alt='info-icon'></img></a>
+            }
+          </div>
+          <div className="dashboard-control">
+           {
+              this.state.page==='message'
+              ? <>
+                <a onClick={()=> this.onLinkClick('message')} href="#"><img src={activeMessage} alt='mesage-icon'></img></a>
+                <span><img className="circle" src={circle} alt='circle-icon'></img></span>
+              </>
+              : <a onClick={()=> this.onLinkClick('message')} href="#"><img src={message} alt='mesage-icon'></img></a>
+            }
+            
+          </div>
+          <div className="dashboard-control">
+            {
+              this.state.page==='profile'
+              ? <>
+                <a onClick={()=> this.onLinkClick('profile')} href="#"><img src={activeProfile} alt='profile-icon'></img></a>
+                <span><img className="circle" src={circle} alt='circle-icon'></img></span>
+              </>
+              : <a onClick={()=> this.onLinkClick('profile')} href="#"><img src={profile} alt='profile-icon'></img></a>
+            }
+          </div>
         </div>
       </div>
     );
