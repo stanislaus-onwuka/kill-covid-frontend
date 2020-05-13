c2d0c300913254f74c389e9188c45f1d7e53a6c1
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-05-13 21:32:30 +0000

    just finished the doctor's home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './pages/login/login.js';
-import Signup from "./pages/signup/sign-up.js";
-import Evaluation from './pages/Evaluation/Evaluation.js';
+import Login from './Pages/login/login.js';
+import Signup from "./Pages/signup/sign-up.js";
+import Evaluation from './Pages/Evaluation/Evaluation.js';
+import DoctorHome from "./Pages/Doctor Home/Doctor-home"
 import './App.css';
 
 
@@ -10,9 +11,10 @@ function App() {
   return (
     <div>
         <Switch>
-          <Route path="/Login" component={Login} />
-          <Route path="/Eval" component={Evaluation} />
-          <Route path="/" component={Signup} />
+        <Route path="/Login" component={Login} />
+        <Route path="/Eval" component={Evaluation} />
+        <Route path="/Doctor-Home" component={DoctorHome} />
+        <Route path="/" component={Signup} />
         </Switch>
     </div>
   );
