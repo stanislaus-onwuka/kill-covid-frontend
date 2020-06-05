953d6b2ca16a86d07549c7af59a711370874e353
Olamide <aboyejiolamide15@gmail.com>
2020-06-06 00:47:19 +0100

    put keys in env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app';
 import "firebase/auth";
-  
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAtmxjKV88E92x7dDWo3AoYiHBUTxkutHU",
-  authDomain: "coveed-19.firebaseapp.com",
-  databaseURL: "https://coveed-19.firebaseio.com",
-  projectId: "coveed-19",
-  storageBucket: "coveed-19.appspot.com",
-  messagingSenderId: "1332829122",
-  appId: "1:1332829122:web:a22b5852298ee0912b945b",
-  measurementId: "G-XTF1VWVKFT"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apppId : process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MESUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
