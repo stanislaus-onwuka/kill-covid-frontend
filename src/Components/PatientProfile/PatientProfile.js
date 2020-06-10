b8a4f6246666066dc47319fe72ebff66372af7de
Onwuka Stanley C <43853290+stanislaus-onwuka@users.noreply.github.com>
2020-06-10 15:08:32 +0000

    Update PatientProfile.js

--- src/Components/PatientProfile/PatientProfile.js
+++ src/Components/PatientProfile/PatientProfile.js
@@ -1,4 +1,4 @@
-import React, { Component } from './node_modules/react';
+import React, { Component } from 'react';
 import userImage from './../../Assets/prof.png';
 import backIcon from './../../Assets/svg/arrow-left.svg';
 import ActivitySchedule from '../ActivitySchedule/ActivitySchedule';
