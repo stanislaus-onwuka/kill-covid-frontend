ee6627832ecd986c5580fea53a9af7e35afb35d8
Onwuka Stanley C <43853290+stanislaus-onwuka@users.noreply.github.com>
2020-06-10 15:11:08 +0000

    Update EvalContent.js

--- src/Components/EvalContent/EvalContent.js
+++ src/Components/EvalContent/EvalContent.js
@@ -1,4 +1,4 @@
-import React,{ Component } from './node_modules/react';
+import React,{ Component } from 'react';
 import {Link} from './node_modules/react-router-dom';
 import './EvalContent.css';
 
