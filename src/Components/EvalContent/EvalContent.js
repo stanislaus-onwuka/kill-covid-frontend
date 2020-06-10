ba7b7758ebb1c2a7bf95e0c02c48fc0c954d458e
Onwuka Stanley C <43853290+stanislaus-onwuka@users.noreply.github.com>
2020-06-10 15:14:40 +0000

    Update EvalContent.js

--- src/Components/EvalContent/EvalContent.js
+++ src/Components/EvalContent/EvalContent.js
@@ -1,5 +1,5 @@
 import React,{ Component } from 'react';
-import {Link} from './node_modules/react-router-dom';
+import {Link} from 'react-router-dom';
 import './EvalContent.css';
 
 let countries =  [
