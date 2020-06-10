30f5db84b3cc57d4736b3f149e852eebdbab9b17
Dro <lorddro1532@gmail.com>
2020-06-10 02:46:04 +0100

    Fix 1

--- src/pages/Patient/Patient.js
+++ src/pages/Patient/Patient.js
@@ -8,7 +8,7 @@ import circle from './../../Assets/svg/circle.svg';
 import info from './../../Assets/svg/info.svg';
 import message from './../../Assets/svg/message.svg';
 import profile from './../../Assets/svg/profile.svg';
-import PatientHome from './../../components/PatientHome/PatientHome';
+import PatientHome from './../../components/PatientHome/PatientHome.js';
 import PatientInfo from './../../components/PatientInfo/PatientInfo';
 import PatientProfile from './../../components/PatientProfile/PatientProfile';
 import PatientConsultation from './../../components/PatientConsultation/PatientConsultation';
